8b60f3ec6881779da23b0eb6c7977f3072234e12
Cao Minh Hiep <hiepcm@gmail.com>
2020-12-13 01:18:36 +0900

    Second commit: create models and migrations

---
+++ server-bussiness-db/app/Team.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Team extends Model
+{
+    protected $table = 'team';
+    protected $fillable = ['id', 'del_flag', 'name', 'leader', 'description', 'created_by', 'modified_by', 'created_at', 'updated_at'];
+}
